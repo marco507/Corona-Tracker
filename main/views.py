a820f2a4c1d7c9eca9381be49110ef2f94f1d396
Marco Schweiss <schweiss.marco@hotmail.com>
2022-05-10 15:49:25 +0200

    Integrated HDF5 download functionality

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
+import mimetypes
 from django.shortcuts import render
+from django.http import HttpResponse
+
 from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models.formatters import DatetimeTickFormatter, BasicTickFormatter
+import pandas as pd
+import os
+
+from scraper.settings import BASE_DIR
 
 from .models import Record
-
-from datetime import datetime, timedelta
 
 
 def index(request):
@@ -18,8 +23,8 @@ def index(request):
         # We init the country dictionary inside the plot dictionary
         plot[country] = {}
 
-        # We retrieve all records for the country
-        records = Record.objects.filter(country=country)
+        # We retrieve all records for the country sorted by ascending date
+        records = Record.objects.filter(country=country).order_by('date')
 
         # From the queryset, we create three lists for the three different data types and a list for the dates
         total_infections = [record.total_infections for record in records]
@@ -60,3 +65,34 @@ def index(request):
             }
 
     return render(request, 'index.html', {'plot': plot})
+
+def download(request):
+
+    # We create an empty HDF5 file
+    hdf5_file = pd.HDFStore('static/data.h5')
+
+    # We iterate through the countries
+    for country in ['Germany', 'Austria', 'Switzerland']:
+        # We retrieve all records for the country sorted by ascending date
+        records = Record.objects.filter(country=country).order_by('date')
+        # We create a dataframe from the queryset.
+        df = pd.DataFrame(list(records.values()), columns=['date', 'total_infections', 'total_deaths', 'incidence'])
+        # We remove the time component from the date
+        df['date'] = df['date'].dt.date
+        # We convert the date to a string
+        df['date'] = df['date'].astype(str)
+        # We convert the incidence column to a float
+        df['incidence'] = df['incidence'].astype(float)
+
+        # We append the dataframe to a HDF5 file, key = country
+        hdf5_file.put(country, df, format='table', data_columns=True)
+
+    # We close the HDF5 file
+    hdf5_file.close()
+
+    # We send the HDF5 file to the browser
+    response = HttpResponse(open('static/data.h5', 'rb'), content_type='application/octet-stream')
+    response['Content-Disposition'] = 'attachment; filename=data.h5'
+
+    return response
+
